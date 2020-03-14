366bca0f3e7824fe7d3f9a897e4f72a093ef76e2
Daniel Micay <danielmicay@gmail.com>
2020-03-14 17:20:37 -0400

    use bundletool from the Chromium tree

--- generate_release.sh
+++ generate_release.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ANDROID_TOP=~/android/grapheneos
 KEYSTORE=$PWD/vanadium.keystore
 APKSIGNER=$PWD/third_party/android_sdk/public/build-tools/29.0.2/apksigner
+BUNDLETOOL=$PWD/build/android/gyp/bundletool.py
 
 cd out/Default/apks
 
 rm -f TrichromeChrome.apks
 rm -f TrichromeChrome.apk
 
-java -jar $ANDROID_TOP/prebuilts/bundletool/bundletool-all-20190905.jar build-apks --bundle TrichromeChrome.aab --output TrichromeChrome.apks --mode=universal --ks $KEYSTORE --ks-pass pass:vanadiumpass --ks-key-alias vanadiumkey
+$BUNDLETOOL build-apks --bundle TrichromeChrome.aab --output TrichromeChrome.apks --mode=universal --ks $KEYSTORE --ks-pass pass:vanadiumpass --ks-key-alias vanadiumkey
 unzip TrichromeChrome.apks universal.apk
 mv universal.apk TrichromeChrome.apk
 
