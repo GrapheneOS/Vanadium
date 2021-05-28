4623897ae4d78a011fe63a14843c15dcf2aa9879
Daniel Micay <danielmicay@gmail.com>
2021-05-27 20:46:47 -0400

    switch to downstream TrichromeChrome apk again

--- generate_release.sh
+++ generate_release.sh
@@ -6,8 +6,6 @@ set -o errexit -o nounset -o pipefail
 
 KEYSTORE=$PWD/../vanadium.keystore
 APKSIGNER=$PWD/third_party/android_sdk/public/build-tools/30.0.1/apksigner
-BUNDLETOOL=$PWD/build/android/gyp/bundletool.py
-AAPT2=$PWD/third_party/android_build_tools/aapt2/aapt2
 
 read -p "Enter keystore passphrase: " -s keystore_pass
 echo
@@ -18,10 +16,6 @@ rm -rf release
 mkdir release
 cd release
 
-$BUNDLETOOL build-apks --aapt2 $AAPT2 --bundle ../TrichromeChrome6432.aab --output TrichromeChrome.apks --mode=universal --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias vanadium <<< $keystore_pass
-unzip TrichromeChrome.apks universal.apk
-mv universal.apk TrichromeChrome.apk
-
-for app in TrichromeLibrary TrichromeWebView; do
+for app in TrichromeChrome TrichromeLibrary TrichromeWebView; do
     $APKSIGNER sign --ks $KEYSTORE --ks-pass file:/dev/stdin --ks-key-alias vanadium --in ../${app}6432.apk --out $app.apk <<< $keystore_pass
 done
